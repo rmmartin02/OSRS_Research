316cac2ba32e4d9b8927f5db95a28200adc4e6f2
Russell Martin <rmmartin02@email.wm.edu>
2019-03-28 16:34:09 -0400

    Standardize it to 1000 points

--- ML/OSRS/RegressionExperiment.py
+++ ML/OSRS/RegressionExperiment.py
@@ -42,71 +42,74 @@ def main():
                 toWrite['item'] = item
 
                 prices = items.getPrices(item)
-                changes = items.getPriceChanges(prices)
-                toWrite['numPrices'] = len(prices)
+                if len(prices)>=1000:
+                    prices = prices[-1000:]
+                    print(len(prices))
+                    changes = items.getPriceChanges(prices)
+                    toWrite['numPrices'] = len(prices)
 
-                featSizes = [21]
-                model = rm.RegressionModel(changes, [changes], featSizes, 'sigmoid', sum(featSizes), sum(featSizes), .8, .9)
+                    featSizes = [21]
+                    model = rm.RegressionModel(changes, [changes], featSizes, 'sigmoid', sum(featSizes), sum(featSizes), .8, .9)
 
-                beforeScore = model.getScore()
-                toWrite['startLoss'] = beforeScore[0]
-                toWrite['startMAE'] = beforeScore[1]
+                    beforeScore = model.getScore()
+                    toWrite['startLoss'] = beforeScore[0]
+                    toWrite['startMAE'] = beforeScore[1]
 
-                model.train(50, 16)
+                    model.train(50, 16)
 
-                afterScore = model.getScore()
-                toWrite['endLoss'] = afterScore[0]
-                toWrite['endMAE'] = afterScore[1]
+                    afterScore = model.getScore()
+                    toWrite['endLoss'] = afterScore[0]
+                    toWrite['endMAE'] = afterScore[1]
 
-                toWrite['numEpochs'] = len(model.getHistory()['loss'])
+                    toWrite['numEpochs'] = len(model.getHistory()['loss'])
 
-                bl = int(items.getInfo(item)['buyLimit'])
-                test_prices = prices[-1 * len(model.y_test) + -1 * len(model.y_val):-1 * len(model.y_test)]
-                budget = test_prices[0]*bl+1
-                y_pred = model.predict(model.x_val)
+                    bl = int(items.getInfo(item)['buyLimit'])
+                    test_prices = prices[-1 * len(model.y_test) + -1 * len(model.y_val):-1 * len(model.y_test)]
+                    budget = test_prices[0]*bl+1
+                    y_pred = model.predict(model.x_val)
 
-                buySigs = [a >= 0 for a in y_pred]
-                sellSigs = [a <= 0 for a in y_pred]
-                profit = ts.modelProfit(buySigs, sellSigs, test_prices, bl, budget)[-1]
-                best = [profit, 0, 0, len([a for a in buySigs if a == True]), len([a for a in sellSigs if a == True])]
+                    buySigs = [a >= 0 for a in y_pred]
+                    sellSigs = [a <= 0 for a in y_pred]
+                    profit = ts.modelProfit(buySigs, sellSigs, test_prices, bl, budget)[-1]
+                    best = [profit, 0, 0, len([a for a in buySigs if a == True]), len([a for a in sellSigs if a == True])]
 
-                for buySig in np.linspace(-1, 1, 20):
-                    for sellSig in np.linspace(1, -1, 20):
-                        buySigs = [a >= buySig for a in y_pred]
-                        sellSigs = [a <= sellSig for a in y_pred]
-                        profit = ts.modelProfit(buySigs, sellSigs, test_prices, bl, budget)[-1]
-                        if profit > best[0]:
-                            best = [profit, buySig, sellSig, len([a for a in buySigs if a == True]),
-                                    len([a for a in sellSigs if a == True])]
+                    for buySig in np.linspace(-1, 1, 20):
+                        for sellSig in np.linspace(1, -1, 20):
+                            buySigs = [a >= buySig for a in y_pred]
+                            sellSigs = [a <= sellSig for a in y_pred]
+                            profit = ts.modelProfit(buySigs, sellSigs, test_prices, bl, budget)[-1]
+                            if profit > best[0]:
+                                best = [profit, buySig, sellSig, len([a for a in buySigs if a == True]),
+                                        len([a for a in sellSigs if a == True])]
 
-                test_prices = prices[-1 * len(model.y_test):]
-                budget = test_prices[0]*bl+1
-                y_pred = model.predict(model.x_test)
+                    test_prices = prices[-1 * len(model.y_test):]
+                    budget = test_prices[0]*bl+1
+                    y_pred = model.predict(model.x_test)
 
-                toWrite['buyLimit'] = bl
-                toWrite['budget'] = budget
-                toWrite['testPrices'] = test_prices
-                temp =  y_pred.tolist()
-                toWrite['predictions'] = temp
+                    toWrite['buyLimit'] = bl
+                    toWrite['budget'] = budget
+                    toWrite['testPrices'] = test_prices
+                    temp =  y_pred.tolist()
+                    toWrite['predictions'] = temp
 
-                buySigs = [a >= best[1] for a in y_pred]
-                sellSigs = [a <= best[2] for a in y_pred]
-                profit = ts.modelProfit(buySigs, sellSigs, test_prices, bl, budget)
-                best = [profit[-1], best[1], best[2], len([a for a in buySigs if a == True]), len([a for a in sellSigs if a == True])]
+                    buySigs = [a >= best[1] for a in y_pred]
+                    sellSigs = [a <= best[2] for a in y_pred]
+                    profit = ts.modelProfit(buySigs, sellSigs, test_prices, bl, budget)
+                    best = [profit[-1], best[1], best[2], len([a for a in buySigs if a == True]), len([a for a in sellSigs if a == True])]
 
-                toWrite['buySignal'] = best[1]
-                toWrite['sellSignal'] = best[2]
-                toWrite['numBuys'] = best[3]
-                toWrite['numSells'] = best[4]
+                    toWrite['buySignal'] = best[1]
+                    toWrite['sellSignal'] = best[2]
+                    toWrite['numBuys'] = best[3]
+                    toWrite['numSells'] = best[4]
 
-                toWrite['model'] = profit[-1]
+                    toWrite['model'] = profit[-1]
 
-                perf, pers, BaH = ts.baselines(test_prices, bl, budget)
-                toWrite['perfect'] = perf[-1]
-                toWrite['persist'] = pers[-1]
-                toWrite['buyAndHold'] = BaH[-1]
+                    perf, pers, BaH = ts.baselines(test_prices, bl, budget)
+                    toWrite['perfect'] = perf[-1]
+                    toWrite['persist'] = pers[-1]
+                    toWrite['buyAndHold'] = BaH[-1]
 
-                info[item] = toWrite
+                    info[item] = toWrite
             except Exception as e:
                 if isinstance(e, KeyboardInterrupt):
                     sys.exit()
@@ -118,7 +121,7 @@ def main():
                     pickle.dump(info,f)
             count+=1
 
-    with open('Results{}{}.pickle'.format(os.sep, num), 'wb') as f:
+    with open('Results{}{}_1000.pickle'.format(os.sep, num), 'wb') as f:
         pickle.dump(info, f)
 
 def createList():

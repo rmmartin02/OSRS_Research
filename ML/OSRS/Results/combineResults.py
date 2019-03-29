2ceddda77c3ffaff57426755604c442acf49dcb7
Russell Martin <rmmartin02@email.wm.edu>
2019-03-29 12:10:44 -0400

    changed stock experiemnt to only use 1000 points

--- ML/OSRS/Results/combineResults.py
+++ ML/OSRS/Results/combineResults.py
@@ -1,15 +1,15 @@
 import pickle
 
-with open("0.pickle",'rb') as f:
+with open("0_1000.pickle",'rb') as f:
 	a = pickle.load(f)
 
-with open("1.pickle",'rb') as f:
+with open("1_1000.pickle",'rb') as f:
         b = pickle.load(f)
 
-with open("2.pickle",'rb') as f:
+with open("2_1000.pickle",'rb') as f:
         c = pickle.load(f)
 
-with open("3.pickle",'rb') as f:
+with open("3_1000.pickle",'rb') as f:
         d = pickle.load(f)
 
 x = {**a, **b}
@@ -17,5 +17,5 @@ y = {**c, **d}
 
 z = {**x, **y}
 
-with open("all.pickle", 'wb') as f:
+with open("all_1000.pickle", 'wb') as f:
 	pickle.dump(z,f)
